48a7dd322e492ccbdd1ac304e2e7760c6d25b442
Nikhil George <nikhil.george98@gmail.com>
2023-11-13 14:24:06 -0600

    Complete Client, Server, DB specs implemented
    Request-response between client and server works as intended. Client caches result in local DB and saves in JSON
    
    Pending visualizer in OCaml and benchmarking

--- project/Client/cypher-lexer-parser/Driver.py
+++ project/Client/cypher-lexer-parser/Driver.py
@@ -6,6 +6,8 @@ from CypherParser import CypherParser
 # from VisitorInterp import VisitorInterp
 import json
 import requests
+import sqlite3
+import os
 
 class parseErrorListener(ErrorListener):
     def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
@@ -20,6 +22,44 @@ class parseErrorListener(ErrorListener):
     def syntaxError(self, recogniser, offendingSymbol, line, charPositionInLine, msg, e):
         print("Query could not be parsed, enter a valid query!")
 
+
+def discardCache(db):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS cache")
+    conn.commit()
+    conn.close()
+
+
+def clearCache(db):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    cur.execute("DELETE FROM cache")
+    conn.commit()
+    conn.close()
+
+
+def writeCache(db, query, result, exec_time):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO cache VALUES(?, ?, ?)", (query, result, exec_time, ))
+    conn.commit()
+    conn.close()
+
+
+def readCache(db, query):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    cur.execute("SELECT result FROM cache WHERE query=?", (query, ))
+    res = cur.fetchone()
+    conn.commit()
+    conn.close()
+    
+    if res:
+        return res[0]
+    else:
+        return None
+    
 
 def main(arg):
     if arg.lower().endswith(('.txt', '.cypher')):
@@ -40,26 +80,67 @@ def main(arg):
 if __name__ == '__main__':
 
     server_url = "http://localhost:8080/query"
+    res_json_file = "/home/nikhil/pp-ng25762/project/Client/data/res.json"
+    
+    localDB = "local.db"
+
+    discardCache(localDB)
+    
+    conn = sqlite3.connect(localDB)
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS cache(query TEXT PRIMARY KEY, result TEXT, exec_time INT)")
+    # cur.execute("CREATE TABLE IF NOT EXISTS benchmark(query TEXT PRIMARY KEY, times_exec INT, total_time INT)")
+    conn.commit()
+    conn.close()
+
+    write_query_keywords = ["CREATE", "SET", "DELETE", "MERGE", "REMOVE"]
 
     while(1):
+        print()
         query = input('Enter a Cypher Query: ')
-        print("Input Query is: ", query)
         errorCount = main(query)
         
         if errorCount>0:
             continue
-        
-        request = {
-            "query": query
-        }
-        request_json = json.dumps(request)
-        print("Request: ", request_json)
 
-        try:
-            response = requests.post(server_url, request_json)
-            result = response.json()
-            print(json.dumps(result))
-        except requests.exceptions.RequestException as e:
-            print("Error is sending request/no response: ", e)
+        cached = readCache(localDB, query)
         
-        # main(sys.argv)
+        if cached is not None:
+            print("Found in cache:")
+            print(cached)
+            result_json = json.dumps(json.loads(resp_json['result']), indent = 2)
+            with open(res_json_file, "w") as f:
+                f.write(result_json)
+        else:
+            request = {
+                "query": query
+            }
+            request_json = json.dumps(request)
+            print("Sending Request: ", request_json)
+
+            try:
+                response = requests.post(server_url, request_json)
+
+                if response.status_code == 200:
+                    resp_json = response.json()
+
+                    if resp_json['status']=='success':
+                        if any(keyword in query.upper() for keyword in write_query_keywords):
+                            print("Write query, clearing cache...")
+                            clearCache(localDB)
+                        else:
+                            print("Read query, caching result: ", resp_json['result'], resp_json['exec_time'])
+                            writeCache(localDB, query, resp_json['result'], resp_json['exec_time'])
+
+                        result_json = json.dumps(json.loads(resp_json['result']), indent = 2)
+                        with open(res_json_file, "w") as f:
+                            f.write(result_json)
+
+                    elif resp_json['status']=='failed':
+                        print(resp_json['result'])
+
+                else:
+                    print("Error: ", response.status_code)
+
+            except requests.exceptions.RequestException as e:
+                print("Error in sending request/no response: ", e)
--- project/Server/main.go
+++ project/Server/main.go
@@ -9,13 +9,20 @@ import (
 	"os"
 	"bufio"
 	"strings"
+	"time"
 )
 
 type Request struct {
 	Query string `json:"query"`
 }
 
-func runQuery(ctx context.Context, address string, username string, password string, query string){
+type Response struct {
+	Status string `json:"status"`
+	Result string `json:"result"`
+	Exec_time time.Duration `json:"exec_time"`
+}
+
+func runQuery(ctx context.Context, address string, username string, password string, query string) (res Response){
 	driver, err := neo4j.NewDriverWithContext(address, neo4j.BasicAuth(username, password, ""))
 	if err != nil {
 		panic(err)
@@ -33,30 +40,49 @@ func runQuery(ctx context.Context, address string, username string, password str
 		neo4j.EagerResultTransformer,
 		neo4j.ExecuteQueryWithDatabase("neo4j"))
 	if err!= nil {
-		panic(err)
+		// panic(err)
+		fmt.Println("Error in query execution: ", err)
+		return Response{Status: "failed", Result: "Error in query execution", Exec_time: -1*time.Second}
+		// return Response{Status: "failed", Result: append(respRes, "Error in query execution"), Exec_time: -1*time.Second}
 	}
 
 	summary := result.Summary
 	records := result.Records
 
+	var respRes []string
+
 	for _, record := range records {
-		fmt.Println(record.AsMap())
+		jsonStr, _ := json.Marshal(record)
+		fmt.Println(string(jsonStr))
+		respRes = append(respRes, string(jsonStr), ",")
 	}
+
+	// resJson, _ := json.Marshal(respRes)
+	// fmt.Println("ResultsJSON: ", string(resJson))
+	if len(respRes) > 1{
+		respRes = respRes[:len(respRes) - 1]
+	}
+	fmt.Println("Results: ", respRes)
 
 	fmt.Printf("The query `%v` returned %v records in %+v.\n",
     summary.Query().Text(), len(records),
     summary.ResultAvailableAfter())
+
+	return Response{Status: "success", Result: fmt.Sprintf("%v", respRes), Exec_time: summary.ResultAvailableAfter()}
+	// return Response{Status: "success", Result: respRes, Exec_time: summary.ResultAvailableAfter()}
 }
 
 
-func run(query string) {
+func run(query string) (res Response){
 	address := "neo4j://localhost:7687"
 	username := "neo4j"
 	password := "neo4jubuntu@2023"
 
 	ctx := context.Background()
 
-	runQuery(ctx, address, username, password, query)
+	res = runQuery(ctx, address, username, password, query)
+
+	return
 }
 
 
@@ -67,8 +93,11 @@ func Query(w http.ResponseWriter, r *http.Request) {
 	if jsonErr != nil {
 		fmt.Println(jsonErr)
 	}
-	fmt.Println(req.Query)
-	run(req.Query)
+	fmt.Println("Received: ", req.Query)
+	res := run(req.Query)
+
+	w.Header().Set("Content-Type", "json")
+	json.NewEncoder(w).Encode(res)
 }
 
 
@@ -76,18 +105,20 @@ func main() {
 
 	fmt.Println("Server Part 1...")
 	//Part 1
-	files, err := os.ReadDir("../Client/cypher-lexer-parser/queries")
+	query_dir := "../Client/cypher-lexer-parser/queries/example/"
+	files, err := os.ReadDir(query_dir)
     if err != nil {
         return
     }
  
     for _, file := range files {
-		if strings.Contains(file.Name(), "test")  || strings.Contains(file.Name(), "fail") {
+		if strings.Contains(file.Name(), "test")  || strings.Contains(file.Name(), "fail") || strings.Contains(file.Name(), "trial" ) {
 			continue
 		}
 		
-		fmt.Println(file.Name())
-		f, err := os.Open("../Client/cypher-lexer-parser/queries/"+file.Name())
+		fmt.Println()
+		fmt.Println("Filename: ", file.Name())
+		f, err := os.Open(query_dir+file.Name())
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -96,13 +127,14 @@ func main() {
 		scanner := bufio.NewScanner(f)
 		for scanner.Scan() {
 		    line := scanner.Text()
-		    // fmt.Println(line) // or do something else with line
+		    // fmt.Println(line)
 			query = query+line
 		}
-		fmt.Println(query)
+		fmt.Println("Query: ", query)
 		run(query)
     }
 	fmt.Println("Finished running preselected queries - Part 1 Complete")
+	fmt.Println()
 
 	fmt.Println("Server Part 2...")
 	//Part 2
